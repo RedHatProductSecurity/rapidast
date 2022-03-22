fd232d64dfe9645506e253525ca8ed4b8fa17bc8
jpweiser <4422366+jpweiser@users.noreply.github.com>
2022-03-22 16:41:52 -0500

    Add containerfile and entrypoint script

---
+++ entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# This MUST be the same as resultDir in config.yaml
+RESULT_DIR=results
+
+# Create rundir
+RUNDIR=$RESULT_DIR/$1
+mkdir $RUNDIR
+
+
+# Run in backgound to make sure we move on to following instructions
+zap.sh -cmd -addonupdate && zap.sh -daemon -port 8090 -config api.key=cnmeemn7jp7ijd8rl5u14q40v8 -config database.newsession=3 -config database.newsessionprompt=false -addoninstall ascanrulesBeta &
+
+# sleep to give ZAP a chance to get set up
+sleep 45
+
+python scripts/gen-zap-script/cli.py --rapidast-config=./config/config.yaml --delete
+python scripts/gen-zap-script/cli.py --rapidast-config=./config/config.yaml --from-yaml scripts/gen-zap-script/rules/software_version_revealed.yaml --load-and-enable
+
+
+# Run scan
+python scripts/apis_scan.py $1

8d8c1c77189b5a76ed6be084390f39c3a1e05e56
Jeremy Bonghwan Choi <jechoi@redhat.com>
2023-01-10 17:45:08 +1000

    Changes on requirements.txt and entrypoint.sh (#48)
    moved requirements.txt and autorun-entrypoint(used for docker image build)

--- scripts/entrypoint_ui.sh
+++ scripts/entrypoint_ui.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-pip install -r config/requirements.txt
+pip install -r requirements.txt
 zap.sh -cmd -config api.key=${API_KEY} -config database.newsession=3 -config database.newsessionprompt=false -addoninstall ascanrulesBeta
 zap.sh -cmd -addonupdate
 zap-webswing.sh

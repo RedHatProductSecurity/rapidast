fbafa315c97b507e518e1f0cf170b785602e323d
Cedric Buissart <cbuissar@redhat.com>
2023-04-20 06:28:02 +0200

    [ZAP] workaround for OAuth2 download of OpenAPI (#74)
    This is aimed at being temporary only.
    If ZAP enables authenticated OpenAPI download, it can be removed.
    Alternatively, the code could be improved slowly in the future.
    
    -> added a `scanners.zap.miscOptions.oauth2OpenapiManualDownload` config
    entry. It defaults to `False`
    -> if the above is True, *and* authentication is OAuth2 *and* there is a
    OpenAPI definition downloaded over URL, then the code downloads it, and
    modifies `apiUrl` to `apiFile`
    -> a new `scanners.downloaders.authenticated_download_with_rtoken()`
    function is provided for this matter.
    
    Note:
    Because of the nature of the action, I don't see how to make a pytest
    for this.
    
    Co-authored-by: cedric <cedric@ltop.lan>

---
+++ scanners/downloaders.py
@@ -0,0 +1,64 @@
+# This is a file with helper functions to download file with authentication.
+import logging
+
+import requests
+import yaml
+
+
+def authenticated_download_with_rtoken(
+    url,
+    dest,
+    rtoken,
+    client_id,
+    auth_url,
+    proxy=None,
+):
+    """Given a URL and Oauth2 authentication parameters, download the URL and store it at `dest`"""
+
+    session = requests.Session()
+
+    # get a token
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/x-www-form-urlencoded",
+    }
+    payload = {
+        "client_id": client_id,
+        "grant_type": "refresh_token",
+        "refresh_token": rtoken,
+    }
+    if proxy:
+        proxy = {
+            "https": f"http://{proxy['proxyHost']}:{proxy['proxyPort']}",
+            "http": f"http://{proxy['proxyHost']}:{proxy['proxyPort']}",
+        }
+
+    resp = session.post(auth_url, data=payload, headers=headers, proxies=proxy)
+
+    if resp.status_code != 200:
+        logging.warning(
+            f"Unable to get a bearer token. Aborting manual download for {url}"
+        )
+        return False
+
+    try:
+        token = yaml.safe_load(resp.text)["access_token"]
+    except Exception as exc:
+        raise RuntimeError(
+            f"Unable to extract access token from OAuth2 authentication:\n {str(exc)}"
+        ) from exc
+    authenticated_headers = {"Authorization": f"Bearer {token}"}
+
+    resp = session.get(url, proxies=proxy, headers=authenticated_headers)
+
+    if resp.status_code >= 400:
+        logging.warning(
+            f"ERROR: download failed with {resp.status_code}. Aborting download for {url}"
+        )
+        return False
+
+    with open(dest, "w") as file:
+        file.write(resp.text)
+
+    logging.debug(f"Successful download of {url} into {dest}")
+    return True

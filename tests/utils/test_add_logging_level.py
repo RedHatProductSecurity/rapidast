dd4355c0c962558c16c2d0eceb0bcdf421b6f89e
Shubh Bapna <sbapna@redhat.com>
2023-06-22 13:50:15 -0400

    lint fixes

--- tests/utils/test_add_logging_level.py
+++ tests/utils/test_add_logging_level.py
@@ -1,13 +1,14 @@
-from utils import add_logging_level
 import logging
 import unittest
+
+from utils import add_logging_level
 
 
 class TestAddLoggingLevel(unittest.TestCase):
     def test_add_logging_level(self):
         add_logging_level("VERBOSE", logging.DEBUG + 5)
         logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.VERBOSE)
-        with self.assertLogs(level='VERBOSE') as cm:
+        with self.assertLogs(level="VERBOSE") as cm:
             logging.verbose("verbose")
             logging.debug("debug")
-        self.assertEqual(cm.output, ['VERBOSE:root:verbose'])
+        self.assertEqual(cm.output, ["VERBOSE:root:verbose"])

40bdf0e86fd09bb4ceee29eca6ad174a805a0170
Jeremy Choi <jechoi@redhat.com>
2023-06-25 08:52:30 -0400

    updated multiuser containerfile: added rapidast(non-root) user, deleted wrapper scripts

--- containerize/run_rapidast.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-# config file should be either absolute path or URL
-CONF=$1
-
-pushd /opt/rapidast/
-./rapidast.py --config "${CONF}"
--- containerize/run_rapidast_debug.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-# in debug mode, only local config file can be used
-CONF=$(realpath $1)
-
-pushd /opt/rapidast/
-./rapidast.py --log-level debug --config "${CONF}"

ab3aef1cf5c3e57a2e064594140b2af40d0cae74
Shubh Bapna <38372682+shubhbapna@users.noreply.github.com>
2023-07-04 19:49:59 -0400

    fix: remove pylint error for logging.verbose (#107)
    fix pylint error for logging.verbose

--- tests/test_load_config_file.py
+++ tests/test_load_config_file.py
@@ -1,11 +1,13 @@
 import unittest
-from unittest.mock import MagicMock, patch
-import rapidast
 import urllib  # pylint: disable=unused-import
+from unittest.mock import MagicMock
+from unittest.mock import patch
+
+import rapidast
 
 
 class TestLoadConfigFile(unittest.TestCase):
-    @patch('urllib.request.urlopen')
+    @patch("urllib.request.urlopen")
     def test_load_config_file_from_url(self, mock_urlopen):
         response = MagicMock()
         response.getcode.return_value = 200
@@ -14,7 +16,7 @@ class TestLoadConfigFile(unittest.TestCase):
         result = rapidast.load_config_file("https://location/config.yaml")
         assert result is response
 
-    @patch('builtins.open')
+    @patch("builtins.open")
     def test_load_config_file_from_local_file(self, mock_open):
         response = MagicMock()
         mock_open.return_value = response

467bad993afa86a1114e113f034fcc136e0c81c6
Jeremy Choi <jechoi@redhat.com>
2023-09-14 22:56:27 -0400

    changed the name of get_token(). improved error handling and log messages

--- exports/defect_dojo.py
+++ exports/defect_dojo.py
@@ -26,7 +26,7 @@ class DefectDojo:
         if token:
             self.headers["Authorization"] = f"Token {token}"
 
-    def get_token(self):
+    def _auth_and_set_token(self):
         """Force a refresh of the token using the username/password"""
         logging.debug("Defect Dojo: refreshing token")
         if not self.username or not self.password:
@@ -45,23 +45,34 @@ class DefectDojo:
 
             self.headers["Authorization"] = f"Token {self.token}"
             logging.debug("Defect Dojo: successfully refreshed token")
+        except requests.exceptions.ConnectTimeout as e:
+            logging.error(
+                f"Getting token failed. Check the URL for defectDojo in config file. err details: {e}"
+            )
+            return 1
         except requests.exceptions.HTTPError as e:
-            logging.warning(f"Defect Dojo export failed: {e}")
-            raise
+            logging.error(
+                f"Getting token failed: Check the username/password for defectDojo in the config file. err details: {e}"
+            )
+            return 1
+
+        return 0
 
     def engagement_exists(self, engagement_id=None, name=None):
         """Return True if an engagement exists, False otherwise
         Engagement is identified either by its name or its ID (positive integer)"""
         if not self.token:
-            self.get_token()
+            self._auth_and_set_token()
         if engagement_id:
             resp = requests.get(
                 f"{self.base_url}/api/v2/engagements/?engagment={engagement_id}",
+                timeout=self.DD_CONNECT_TIMEOUT,
                 headers=self.headers,
             )
         elif name:
             resp = requests.get(
                 f"{self.base_url}/api/v2/engagements/?name={parse.quote_plus(name)}",
+                timeout=self.DD_CONNECT_TIMEOUT,
                 headers=self.headers,
             )
         else:
@@ -89,18 +100,30 @@ class DefectDojo:
             raise ValueError(f"Missing required entries for reimport: {missing}")
 
         if not self.token:
-            self.get_token()
+            if self._auth_and_set_token() == 1:
+                # failed to get token
+                return 1
 
         resp = requests.post(
             endpoint,
+            timeout=self.DD_CONNECT_TIMEOUT,
             headers=self.headers,
             data=data,
             files={"file": open(filename, "rb")},  # pylint: disable=consider-using-with
         )
         if resp.status_code >= 400:
-            print(vars(resp))
+            logging.debug(vars(resp))
             err = resp.json()
-            logging.warning(f"Error while exporting ({resp.status_code}, {err})")
+            logging.error(f"Error while exporting ({resp.status_code}, {err})")
+
+            if "Invalid token" in err["detail"]:
+                logging.error(
+                    "Please check your token in 'config.defectDojo' of the config file"
+                )
+
+            return 1
+
+        return 0
 
     def reimport_scan(self, data, filename):
         """Reimport to an existing engagement with an existing compatible scan."""
@@ -114,7 +137,9 @@ class DefectDojo:
                 "Reimport needs to identify an existing test (by ID or names of product+engagement+test)"
             )
 
-        self._private_import(f"{self.base_url}/api/v2/reimport-scan/", data, filename)
+        return self._private_import(
+            f"{self.base_url}/api/v2/reimport-scan/", data, filename
+        )
 
     def import_scan(self, data, filename):
         """Import to an existing engagement."""
@@ -126,7 +151,9 @@ class DefectDojo:
                 "Import needs to identify an existing engagement (by ID or names of product+engagement)"
             )
 
-        self._private_import(f"{self.base_url}/api/v2/import-scan/", data, filename)
+        return self._private_import(
+            f"{self.base_url}/api/v2/import-scan/", data, filename
+        )
 
     def import_or_reimport_scan(self, data, filename):
         """Decide wether to import or reimport. Based on:
@@ -136,9 +163,9 @@ class DefectDojo:
         if not data or not filename:
             # missing data means nothing to do
             logging.debug("Insufficient data for Defect Dojo")
-            return
+            return 1
 
         if data.get("test"):
-            self.reimport_scan(data, filename)
+            return self.reimport_scan(data, filename)
         else:
-            self.import_scan(data, filename)
+            return self.import_scan(data, filename)
--- tests/test_defectdojo_integration.py
+++ tests/test_defectdojo_integration.py
@@ -10,18 +10,18 @@ def test_dd_invalid_url_scheme():
         defect_d = DefectDojo("invalid_url")
 
 
-def test_dd_get_token_no_username():
+def test_dd_auth_and_set_token_no_username():
     defect_d = DefectDojo("https://127.0.0.1:12345")
     with pytest.raises(ValueError) as e_info:
-        defect_d.get_token()
+        defect_d._auth_and_set_token()
 
     assert "A username and a password are required" in str(e_info)
 
 
-def test_dd_get_token_non_existent_url():
+def test_dd_auth_and_set_token_non_existent_url():
     # assuming 127.0.0.1:12345 is non-existent
     defect_d = DefectDojo(
         "https://127.0.0.1:12345", "random_username", "random_password", "random_token"
     )
     with pytest.raises(requests.exceptions.ConnectionError):
-        defect_d.get_token()
+        defect_d._auth_and_set_token()

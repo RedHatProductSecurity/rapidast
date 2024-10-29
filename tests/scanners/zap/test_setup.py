edf666b36a81f506e781a79ab39bb4f2904bb24e
Camilo Cota <1499184+ccronca@users.noreply.github.com>
2024-10-29 09:04:58 +0100

    Export the ZAP site tree as a JSON file (#229)
    Export the ZAP site tree as a JSON file
    
    See:  https://github.com/RedHatProductSecurity/rapidast/pull/229

--- tests/scanners/zap/test_setup.py
+++ tests/scanners/zap/test_setup.py
@@ -1,4 +1,5 @@
 import os
+import pathlib
 import re
 from pathlib import Path
 
@@ -404,3 +405,36 @@ def test_get_update_command(test_config):
     assert "-addonupdate" in test_zap.get_update_command()
     assert "pluginA" in test_zap.get_update_command()
     assert "pluginB" in test_zap.get_update_command()
+
+
+# Export Site Tree
+
+
+def test_setup_export_site_tree(test_config, pytestconfig):
+    test_zap = ZapNone(config=test_config)
+    test_zap.setup()
+
+    add_script = None
+    run_script = None
+    add_variable_script = None
+    run_variable_script = None
+
+    for item in test_zap.automation_config["jobs"]:
+        if item["name"] == "export-site-tree-add":
+            add_script = item
+        if item["name"] == "export-site-tree-run":
+            run_script = item
+        if item["name"] == "export-site-tree-filename-global-var-add":
+            add_variable_script = item
+        if item["name"] == "export-site-tree-filename-global-var-run":
+            run_variable_script = item
+
+    assert add_script and run_script and add_variable_script and run_variable_script
+
+    assert add_script["parameters"]["name"] == run_script["parameters"]["name"]
+    assert add_script["parameters"]["file"] == f"{pytestconfig.rootpath}/scanners/zap/scripts/export-site-tree.js"
+    assert add_script["parameters"]["engine"] == "ECMAScript : Oracle Nashorn"
+
+    assert add_variable_script["parameters"]["name"] == run_variable_script["parameters"]["name"]
+    assert add_variable_script["parameters"]["inline"]
+    assert add_variable_script["parameters"]["engine"] == "ECMAScript : Graal.js"

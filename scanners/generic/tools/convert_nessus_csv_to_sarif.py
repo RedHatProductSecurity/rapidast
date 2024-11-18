f228df3bf39a6398f4b608fe878f6ff32929db77
J.P. Weiser <4422366+jpweiser@users.noreply.github.com>
2024-11-18 14:48:20 -0500

    Update convert_nessus_csv_to_sarif.py per linter

--- scanners/generic/tools/convert_nessus_csv_to_sarif.py
+++ scanners/generic/tools/convert_nessus_csv_to_sarif.py
@@ -1,31 +1,32 @@
 #!/usr/bin/env python3
-#######################################
-#
+"""
 # Convert a Nessus CSV report to SARIF format(stdout).
 # A usage example (see options in the code):
 #  $ convert_nessus_csv_to_sarify.py [-f <input.csv>] [--log-level=DEBUG]
 # If `-f` is absent, or its value is `-`, CSV data will be read from STDIN
 #
-#
+"""
 import argparse
+import csv
 import json
 import logging
-import sys
-import csv
 import re
+import sys
+
 
 def map_level(risk):
     """
     Map severity to match SARIF level property
     """
-    if risk is 'Critical' or risk is 'High':
+    if risk is "Critical" or risk is "High":
         return "error"
-    elif risk is 'Medium':
+    elif risk is "Medium":
         return "warning"
-    elif risk is 'Low':
+    elif risk is "Low":
         return "note"
     else:
         return "none"
+
 
 def nessus_info(field_name, entry):
     """
@@ -33,7 +34,8 @@ def nessus_info(field_name, entry):
     """
     # Match the field name with RegEx, then split it to extract
     # the value. Finally, strip all surrounding whitespace
-    return re.compile(field_name + '.*\n').search(entry)[0].split(':')[1].strip()
+    return re.compile(field_name + ".*\n").search(entry)[0].split(":")[1].strip()
+
 
 def is_file(file_name):
     """
@@ -41,15 +43,17 @@ def is_file(file_name):
     """
     return file_name is not None and file_name != "-"
 
+
 def uri(host, port):
     """
     Format URI from host and port
     """
-    uri = host
+    target = host
     # Ignore port if 0
-    if port is not '0':
-        uri = uri + ':' + port
-    return uri
+    if port is not "0":
+        target = target + ":" + port
+    return target
+
 
 def convert_csv_to_sarif(csv_file):
     """
@@ -68,22 +72,30 @@ def convert_csv_to_sarif(csv_file):
     }
 
     rule_ids = set()
-    
+
     # Below used for logging purposes for file vs stdin
     if is_file(csv_file):
-        logging.debug(f"Reading input from '{csv_file}'")
+        logging.debug("Reading input from: %s", csv_file)
     else:
         logging.debug("Reading input from STDIN")
 
-    with open(csv_file, newline='') if is_file(csv_file) else sys.stdin as report:
+    with (
+        open(csv_file, newline="", encoding="utf-8") if is_file(csv_file) else sys.stdin
+    ) as report:
         reader = csv.DictReader(report)
         for row in reader:
-            if row['Plugin ID'] == '19506':
+            if row["Plugin ID"] == "19506":
                 # This Plugin contains lots of details about scan to populate SARIF tool property
-                sarif_template["runs"][0]["tool"]["driver"]["name"] = nessus_info('Scanner edition used', row['Plugin Output'])
-                sarif_template["runs"][0]["tool"]["driver"]["version"] = nessus_info('Nessus version', row['Plugin Output'])
+                sarif_template["runs"][0]["tool"]["driver"]["name"] = nessus_info(
+                    "Scanner edition used", row["Plugin Output"]
+                )
+                sarif_template["runs"][0]["tool"]["driver"]["version"] = nessus_info(
+                    "Nessus version", row["Plugin Output"]
+                )
                 # Adding fullname to include policy
-                sarif_template["runs"][0]["tool"]["driver"]["fullName"] = f"{nessus_info('Scanner edition used',row['Plugin Output'])} {nessus_info('Nessus version', row['Plugin Output'])} {nessus_info('Scan policy used', row['Plugin Output'])} Policy"
+                sarif_template["runs"][0]["tool"]["driver"][
+                    "fullName"
+                ] = f"{nessus_info('Scanner edition used',row['Plugin Output'])} {nessus_info('Nessus version', row['Plugin Output'])} {nessus_info('Scan policy used', row['Plugin Output'])} Policy"
 
             if row["Plugin ID"] not in rule_ids:
                 new_rule = {
@@ -93,14 +105,22 @@ def convert_csv_to_sarif(csv_file):
                 }
                 sarif_template["runs"][0]["tool"]["driver"]["rules"].append(new_rule)
                 rule_ids.add(row["Plugin ID"])
-            
-            artifact_location = uri(row["Host"],row["Port"])
-            
+
+            artifact_location = uri(row["Host"], row["Port"])
+
             new_report = {
                 "ruleId": row["Plugin ID"],
                 "level": map_level(row["Risk"]),
-                "message": {"text": f"{row["Plugin Output"]}\n\nSolution: {row["Solution"]}"},
-                "locations": [{"physicalLocation": {"artifactLocation": {"uri": artifact_location}}}],
+                "message": {
+                    "text": f"{row["Plugin Output"]}\n\nSolution: {row["Solution"]}"
+                },
+                "locations": [
+                    {
+                        "physicalLocation": {
+                            "artifactLocation": {"uri": artifact_location}
+                        }
+                    }
+                ],
             }
 
             sarif_template["runs"][0]["results"].append(new_report)
@@ -109,8 +129,13 @@ def convert_csv_to_sarif(csv_file):
 
 
 def main():
+    """
+    Parses arguments before converting Nessus CSV report to SARIF JSON format
+    """
     # Parse command-line arguments
-    parser = argparse.ArgumentParser(description="Convert JSON data to SARIF format with JSON block added to message.")
+    parser = argparse.ArgumentParser(
+        description="Convert JSON data to SARIF format with JSON block added to message."
+    )
     parser.add_argument(
         "-f",
         "--filename",

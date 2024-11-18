60bd037abf84177d13834d1ab27da1dcebaf75fc
Sam <sfowler@redhat.com>
2024-11-18 16:54:15 +1000

    decrease nessus cpu more

--- e2e-tests/test_nessus.py
+++ e2e-tests/test_nessus.py
@@ -9,10 +9,6 @@ class TestNessus(TestBase):
     def test_nessus(self):
         """Test rapidast find expected number of findings in VAPI"""
         self.create_from_yaml(f"{self.tempdir}/nessus-deployment.yaml")
-        os.system("oc get deploy")
-        os.system("oc get deploy -o yaml")
-        os.system("oc get pods")
-        os.system("oc get pods -o yaml")
         self.create_from_yaml(f"{self.tempdir}/nessus-service.yaml")
         wait_until_ready(label_selector="app=nessus", timeout=300)  # nessus is slow to pull and start
 

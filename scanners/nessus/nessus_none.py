2c55b4fb5f6af009fb23a96b7fa9b9d79ae4c7a9
Jon <4422366+jpweiser@users.noreply.github.com>
2024-11-25 10:32:28 -0500

    Adjust line length to 120

--- scanners/nessus/nessus_none.py
+++ scanners/nessus/nessus_none.py
@@ -94,9 +94,7 @@ class Nessus(RapidastScanner):
         return self._scan_id
 
     def setup(self):
-        logging.debug(
-            f"Creating new scan named {self.cfg.scan.folder}/{self.cfg.scan.name}"
-        )
+        logging.debug(f"Creating new scan named {self.cfg.scan.folder}/{self.cfg.scan.name}")
         self._scan_id = self.nessus_client.new_scan(
             name=self.cfg.scan.name,
             targets=self.cfg.scan.targets_as_str(),
@@ -111,9 +109,7 @@ class Nessus(RapidastScanner):
         # created with the name used in the config as a prerequisite
         if self.cfg.scan.policy:
             logging.debug(f"Setting scan policy to {self.cfg.scan.policy}")
-            self.nessus_client.set_scan_policy(
-                scan_id=self.scan_id, policy=self.cfg.scan.policy
-            )
+            self.nessus_client.set_scan_policy(scan_id=self.scan_id, policy=self.cfg.scan.policy)
 
         self.state = State.READY
 
@@ -129,14 +125,9 @@ class Nessus(RapidastScanner):
 
         # Wait for the scan to complete
         start = time.time()
-        while (
-            self.nessus_client.get_scan_status(self.scan_id)["status"]
-            not in END_STATUSES
-        ):
+        while self.nessus_client.get_scan_status(self.scan_id)["status"] not in END_STATUSES:
             if time.time() - start > self.cfg.scan.timeout:
-                logging.error(
-                    f"Timeout {self.cfg.scan.timeout}s reached waiting for scan to complete"
-                )
+                logging.error(f"Timeout {self.cfg.scan.timeout}s reached waiting for scan to complete")
                 self.state = State.ERROR
                 break
 
@@ -148,9 +139,7 @@ class Nessus(RapidastScanner):
         # After scan is complete, download report in csv, nessus, and html format
         # Path and any folders must already exist in this implementation
         logging.debug("Retrieving scan reports")
-        scan_reports = self.nessus_client.get_scan_reports(
-            self.scan_id, self.results_dir
-        )
+        scan_reports = self.nessus_client.get_scan_reports(self.scan_id, self.results_dir)
         logging.debug(scan_reports)
         # Get filename
         for file in glob.glob(self.results_dir + "/*.csv"):

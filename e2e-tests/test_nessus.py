75bb31d69d4da192db653eb15bb9edd4f2c47c81
Camilo Cota <ccota@redhat.com>
2025-03-12 10:47:11 +0100

    Add some assertions and increase timeout in test_nessus

--- e2e-tests/test_nessus.py
+++ e2e-tests/test_nessus.py
@@ -1,5 +1,6 @@
 import os
 
+from conftest import is_pod_with_field_selector_successfully_completed  # pylint: disable=E0611
 from conftest import tee_log  # pylint: disable=E0611
 from conftest import TestBase  # pylint: disable=E0611
 from conftest import wait_until_ready  # pylint: disable=E0611
@@ -10,11 +11,16 @@ class TestNessus(TestBase):
         """Test rapidast find expected number of findings in VAPI"""
         self.create_from_yaml(f"{self.tempdir}/nessus-deployment.yaml")
         self.create_from_yaml(f"{self.tempdir}/nessus-service.yaml")
-        wait_until_ready(label_selector="app=nessus", timeout=300)  # nessus is slow to pull and start
+        assert wait_until_ready(label_selector="app=nessus", timeout=300)  # nessus is slow to pull and start
 
         self.create_from_yaml(f"{self.tempdir}/rapidast-nessus-configmap.yaml")
         self.create_from_yaml(f"{self.tempdir}/rapidast-nessus-pod.yaml")
-        wait_until_ready(field_selector="metadata.name=rapidast-nessus")
+        # @FIX: We don't assert the container's successful completion because it
+        # ends up in an 'Error' status. This happens because the configuration provided
+        # defines authentication, but the Nessus scanner doesn't support authentication
+        is_pod_with_field_selector_successfully_completed(
+            field_selector="metadata.name=rapidast-nessus", timeout=300  # llm-based image takes really long to download
+        )
 
         logfile = os.path.join(self.tempdir, "rapidast-nessus.log")
         tee_log("rapidast-nessus", logfile)

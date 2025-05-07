eed5b74b0a8a09f98c1c054636c7621e50eb3890
Camilo Cota <ccota@redhat.com>
2025-05-07 15:08:32 +0200

    RapiDAST version generation based on Git tags

---
+++ version.py
@@ -0,0 +1,57 @@
+import os
+from git import Repo
+from setuptools_scm import version as _version, ScmVersion
+
+def get_current_branch():
+    """Gets the name of the current Git branch"""
+    try:
+        repo_path = os.getcwd()
+        repo = Repo(repo_path)
+        return repo.active_branch.name
+    except Exception as e:
+        print(f"Error getting current branch: {e}")
+        return None
+
+
+def get_latest_main_tag() -> str:
+    """Gets the latest tag name from the "main" branch"""
+    try:
+        repo_path = os.getcwd()
+        repo = Repo(repo_path)
+        current_branch = get_current_branch()
+        main_branch = repo.heads.main
+        repo.git.checkout(main_branch)
+        tags = sorted(repo.tags, key=lambda t: t.commit.committed_date, reverse=True)
+        repo.git.checkout(current_branch)
+
+        if tags:
+            return str(tags[0])
+        return None
+
+    except Exception as e:
+        print(f"Error getting latest tag info: {e}")
+        return None
+
+
+def custom_local_scheme(version: ScmVersion)-> str:
+    if version.branch == "main":
+        return ""
+
+    return f"+{version.node[1:8]}-{version.node_date.strftime('%Y%m%d')}"
+
+
+def custom_version_scheme(version: ScmVersion)-> str:
+
+    latest_tag_name = get_latest_main_tag()
+
+    fallback_tag = "0.1.0"
+
+    if latest_tag_name:
+        version.tag = latest_tag_name
+    else:
+        return f"{fallback_tag}"
+
+    if version.branch == "main":
+        return f"{version.format_with(latest_tag_name)}"
+
+    return f"{version.format_next_version(_version.guess_next_simple_semver, retain=_version.SEMVER_PATCH, fmt='{guessed}')}"

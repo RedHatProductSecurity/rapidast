59213991748c0e07c6ca1945fce43ccb3a5b12ab
Camilo Cota <ccota@redhat.com>
2025-05-07 16:06:42 +0200

    Fix linting issues

--- version.py
+++ version.py
@@ -1,6 +1,9 @@
 import os
+
 from git import Repo
-from setuptools_scm import version as _version, ScmVersion
+from setuptools_scm import ScmVersion
+from setuptools_scm import version as _version
+
 
 def get_current_branch():
     """Gets the name of the current Git branch"""
@@ -8,7 +11,7 @@ def get_current_branch():
         repo_path = os.getcwd()
         repo = Repo(repo_path)
         return repo.active_branch.name
-    except Exception as e:
+    except Exception as e:  # pylint: disable=W0718
         print(f"Error getting current branch: {e}")
         return None
 
@@ -28,20 +31,19 @@ def get_latest_main_tag() -> str:
             return str(tags[0])
         return None
 
-    except Exception as e:
+    except Exception as e:  # pylint: disable=W0718
         print(f"Error getting latest tag info: {e}")
         return None
 
 
-def custom_local_scheme(version: ScmVersion)-> str:
+def custom_local_scheme(version: ScmVersion) -> str:
     if version.branch == "main":
         return ""
 
     return f"+{version.node[1:8]}-{version.node_date.strftime('%Y%m%d')}"
 
 
-def custom_version_scheme(version: ScmVersion)-> str:
-
+def custom_version_scheme(version: ScmVersion) -> str:
     latest_tag_name = get_latest_main_tag()
 
     fallback_tag = "0.1.0"
@@ -54,4 +56,4 @@ def custom_version_scheme(version: ScmVersion)-> str:
     if version.branch == "main":
         return f"{version.format_with(latest_tag_name)}"
 
-    return f"{version.format_next_version(_version.guess_next_simple_semver, retain=_version.SEMVER_PATCH, fmt='{guessed}')}"
+    return version.format_next_version(_version.guess_next_simple_semver, retain=_version.SEMVER_PATCH, fmt="{guessed}")

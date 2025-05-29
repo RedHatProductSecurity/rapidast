483ebe5a2f7cfecb8066b545e19d5dc592a4aa50
Camilo Cota <ccota@redhat.com>
2025-05-29 11:36:04 +0200

    Config dataclasses for Generic scanner configuration

---
+++ configmodel/models/scanners/generic.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass, field
+from typing import List, Optional
+from ..general import ContainerType
+
+@dataclass
+class GenericContainerParameters:
+    image: Optional[str] = None
+    command: Optional[str] = None
+    validReturns: Optional[str] = None
+    podName: Optional[str] = None
+    volumes: Optional[List[str]] = field(default_factory=list)
+
+@dataclass
+class GenericContainer:
+    type: ContainerType = ContainerType.PODMAN
+    parameters: Optional[GenericContainerParameters] = None
+
+@dataclass
+class GenericConfig:
+    inline: Optional[str] = None
+    results: Optional[str] = None
+    toolDir: Optional[str] = None
+    container: Optional[GenericContainer] = None
--- scanners/generic/generic.py
+++ scanners/generic/generic.py
@@ -3,6 +3,10 @@ import os
 import pprint
 import shutil
 
+import dacite
+
+from configmodel.models.general import ContainerType
+from configmodel.models.scanners.generic import GenericConfig
 from scanners import RapidastScanner
 from scanners import State
 
@@ -37,6 +41,21 @@ class Generic(RapidastScanner):
 
         # The command to run (excluding the "container" layer)
         self.generic_cli = []
+
+        generic_config_section = config.subtree_to_dict(f"scanners.{ident}")
+        if generic_config_section is None:
+            raise ValueError(f"'scanners.{ident}' section not in config")
+
+        dacite_config = dacite.Config(
+            type_hooks={
+                # Dacite doesn't natively support enums, so we use `type_hooks` as a workaround
+                # to properly resolve enum values
+                # https://github.com/konradhalas/dacite/issues/61
+                ContainerType: ContainerType
+            }
+        )
+
+        self.cfg = dacite.from_dict(data_class=GenericConfig, data=generic_config_section, config=dacite_config)
 
     ###############################################################
     # PUBLIC METHODS                                              #
